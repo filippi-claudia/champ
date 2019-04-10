3ea3b4543f9caee1fb830d03eb9ed16e41bd837d
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2019-04-10 19:12:03 +0200

    Use allocatable arrays in lbfgs_iter

--- vmc/lbfgs_iter.f
+++ vmc/lbfgs_iter.f
@@ -8,14 +8,13 @@
       include 'sr.h'
 
       common /sr_mat_n/ sr_o(MPARM,MCONF),sr_ho(MPARM,MCONF),obs(MOBS,MSTATES),s_diag(MPARM,MSTATES),s_ii_inv(MPARM),h_sr(MPARM),wtg(MCONF,MSTATES),elocal(MCONF,MSTATES),jfj,jefj,jhfj,nconf
-      ! need energy for LBFGS iteration
-      ! TODO figure out a cleaner way to get eold (as a subroutine 
-      ! parameter is best)
 
       dimension deltap(*), parameters(*), diag(*), workspace(*)
 
       real(kind=8), dimension(1), allocatable :: parameters_old(:)
       real(kind=8), dimension(1), allocatable :: parms_lbfgs(:)
+      !dimension parameters_old(MPARM)
+      !dimension parms_lbfgs(MPARM)
 
       allocate(parameters_old(nparm))
       allocate(parms_lbfgs(nparm))
@@ -23,8 +22,7 @@
       parms_lbfgs = parameters(1:nparm)
       parameters_old = parms_lbfgs
 
-      ! save old parameters to compute deltap after lbfgs step
-      parameters_old = parameters(1:MPARM)
+      !TODO: Figure out why h_sr is filled with NaN
 
       ! TODO: make num_history configurable
       call lbfgs_iteration(energy, parms_lbfgs, -h_sr, nparm, 5, diag, workspace)
