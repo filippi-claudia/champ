737ed7bc98cb97240924719a756b05b972b22e34
Jonas Feldt <j.feldt@utwente.nl>
2019-05-15 06:04:54 +0200

    Added additional testjob: butatdiene GS, SR for 3 differently sized wavefunctions.

---
+++ tests/butadiene-3wfs/include/vmc.h
@@ -0,0 +1,26 @@
+c MELEC  >= number of electrons
+c MORB   >= number of orbitals
+c MBASIS >= number of basis functions
+c MDET   >= number of determinants
+c MCENT  >= number of centers
+c MCTYPE >= number of center types
+c MCTYP3X=max(3,MCTYPE)
+
+c Slater matrices are dimensioned (MELEC/2)**2 assuming 
+c equal numbers of up and down spins. MELEC has to be 
+c correspondingly larger if spin polarized calculations 
+c are attempted.
+
+      integer MELEC,MORB,MBASIS,MDET,MCENT,MCTYPE,MCTYP3X,
+     &NSPLIN,nrad,MORDJ,MORDJ1,MMAT_DIM,MMAT_DIM2
+
+      real*8 radmax,delri
+
+      character*20 method
+
+      parameter(MELEC=50,MORB=500,MBASIS=500,MDET=40000,MCENT=10,MCTYPE=5,
+     &MCTYP3X=5,NSPLIN=1001,MORDJ=7,radmax=10.d0,nrad=3001,
+     &MMAT_DIM=(MELEC*MELEC)/4,MMAT_DIM2=(MELEC*(MELEC-1))/2,
+     &MORDJ1=MORDJ+1,delri=(nrad-1)/radmax)
+
+      common /method_opt/ method
