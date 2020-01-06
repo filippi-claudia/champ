b6283013841e554df9e68b79b230883d26c3a733
lopeztarifa <pablo.lopez.tarifa@gmail.com>
2020-01-06 14:19:39 +0100

    Adding specific include files for psb3_cas66 test.

---
+++ tests/psb3_cas66/include/README.md
@@ -0,0 +1,1 @@
+psb3_cas66 test has to be run with these includes
---
+++ tests/psb3_cas66/include/optci.h
@@ -0,0 +1,10 @@
+c     flags and dimensions for generalized CI expectation values
+c
+c     maximal number of terms, max dim of reduced matrices
+      parameter (MXCITERM=4600,MXCIREDUCED=2000,MXCIMATDIM=MXCITERM*(MXCIREDUCED+1)/2)
+c     ici  : flag whether CI should be sampled
+c     nciterm: number of terms (possibly after a transformation)
+c     nciterm2:number of terms for Nightingale's method (EL(i) O(k))
+c     nciprim: number of primitive terms (determinant ratios)
+c     iciprt : print flag 
+      common /ci000/ nciterm, nciprim, iciprt
