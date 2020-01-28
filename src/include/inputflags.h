b5eeb0d4b65feb0af4a0cbb20cda05d1f46d4b29
lopeztarifa <pablo.lopez.tarifa@gmail.com>
2020-01-28 14:38:56 +0100

    modif inputflags.h format

--- src/include/inputflags.h
+++ src/include/inputflags.h
@@ -1,4 +1,4 @@
-c indicate if blocks found in input file
+! indicate if blocks found in input file
       common /inputflags/ iznuc,igeometry,ibasis_num,ilcao,iexponents
       common /inputflags/ ideterminants,ijastrow_parameter, ioptorb_def,ilattice
       common /inputflags/ ici_def,iforces,icsfs,imstates,igradients,icharge_efield
