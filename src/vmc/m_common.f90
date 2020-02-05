e63bf7dcd2aeabd0aab9d665998970559f358a32
lopeztarifa <pablo.lopez.tarifa@gmail.com>
2020-02-05 14:45:32 +0100

    Adding private statements to m_common.f90

---
+++ src/vmc/m_common.f90
@@ -0,0 +1,73 @@
+!> \brief File collecting all modules that replace common blocks.  
+!>
+!> \author P. Lopez-Tarifa NLeSC(2019)
+! Victor's suggestion
+!module vmc
+!  include 'vmc.h'
+!end module
+
+module precision_kinds
+  ! named constants for 4, 2, and 1 byte integers:
+  integer, parameter :: &
+       i4b = selected_int_kind(9), &
+       i2b = selected_int_kind(4), &
+       i1b = selected_int_kind(2)
+  ! single, double and quadruple precision reals:
+  integer, parameter :: &
+       sp = kind(1.0), &
+       dp = selected_real_kind(2 * precision(1.0_sp)), &
+       qp = selected_real_kind(2 * precision(1.0_dp))
+end module precision_kinds
+
+ module atom
+   !> Arguments: znuc, cent, pecent, iwctype, nctype, ncent
+   use precision_kinds, only: dp  
+
+   include 'vmc.h'
+
+   real(dp) :: cent( 3, MCENT)
+   real(dp) :: znuc( MCTYPE)
+   real(dp) :: pecent
+   integer  :: iwctype( MCENT), nctype, ncent
+
+   private
+   public   :: znuc, cent, pecent, iwctype, nctype, ncent 
+   save
+ end module atom
+   
+ module contrl_per
+   !> Arguments: iperiodic, ibasis 
+
+   integer  :: iperiodic, ibasis
+
+   private
+   public   :: iperiodic, ibasis
+   save
+ end module contrl_per
+ 
+ module da_pseudo
+   !> Arguments: da_pecent, da_vps, da_nonloc  
+
+   use precision_kinds, only: dp  
+
+   include 'vmc.h'
+   include 'pseudo.h'
+
+   real(dp) :: da_pecent( 3, MCENT), da_vps( 3, MELEC, MCENT, MPS_L)
+   real(dp) :: da_nonloc( 3, MCENT)= 0.0D0 
+
+   private
+   public   :: da_pecent, da_vps, da_nonloc 
+   save
+ end module da_pseudo 
+ 
+ module force_analy 
+   !> Arguments: iforce_analy 
+
+   integer  :: iforce_analy 
+
+   private
+   public   :: iforce_analy 
+   save
+ end module force_analy 
+ 
