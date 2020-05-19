7af816867970b63ea1c066211a3f9b7db25bd66b
lopeztarifa <pablo.lopez.tarifa@gmail.com>
2020-05-19 17:00:31 +0200

    Deleting common blocks /tab00X/ from embed.h (for good).

--- src/include/embed.h
+++ src/include/embed.h
@@ -8,16 +8,8 @@
 !     max number of gridpoints in exchange tables
       parameter (MXXGRID=1)
 !     flags whether tables are used and interpolation order
-      common /tab000/ ihtree,ixchng,ihpol,ixpol
 !     number of tables,  table sizes
-      common /tab001/ nhtab,nxtab,ndimh(3,MXHTAB),ndimx(3,MXXTAB)
 !     table bounds and spacing
-      common /tab002/ rminh(3,MXHTAB), rmaxh(3,MXHTAB),drh(3,MXHTAB)
-      common /tab003/ rminx(3,MXXTAB), rmaxx(3,MXXTAB),drx(3,MXXTAB)
 !     tables
-      common /tab004/ thtree(MXHGRID*MXHGRID*MXHGRID,MXHTAB)
-      common /tab005/ txchng(MXXGRID*MXXGRID*MXXGRID,MXXTAB)
 !     evaluation count
-      common /tab006/ nevalu(2,MXTAB)
 !     used part of tables 
-      common /tab007/ tmin(3,MXTAB),tmax(3,MXTAB)
