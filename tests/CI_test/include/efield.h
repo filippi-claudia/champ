d9c34f62ba4af937b967e226fef807fa0785ccaa
lopeztarifa <pablo.lopez.tarifa@gmail.com>
2020-06-25 23:14:22 +0200

    Deleting common block /efield_blk/ from efield.h .

--- tests/CI_test/include/efield.h
+++ tests/CI_test/include/efield.h
@@ -2,5 +2,3 @@
       parameter(MCHARGES=100)
 
       common /efield/ iefield,ncharges,iscreen
-      common /efield_blk/ xcharge(MCHARGES),ycharge(MCHARGES),zcharge(MCHARGES)
-      common /efield_blk/ qcharge(MCHARGES),ascreen(MCHARGES),bscreen(MCHARGES)
