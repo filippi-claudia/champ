e44a956bad9d1314cf2cf2c2857eb882535ebe54
lopeztarifa <pablo.lopez.tarifa@gmail.com>
2020-09-05 18:05:04 +0200

    Delete common /pcm_ameta/, /pcm_ah/ and /pcm_pot/ .

--- tests/CI_test/include/pcm.h
+++ tests/CI_test/include/pcm.h
@@ -1,7 +1,5 @@
       parameter (MCHS=1,MCHV=1,MSPHERE=30)
-      common /pcm_ah/ahca(MCHS,MCHS),bh(MCHS)
 
-      common /pcm_pot/ penups,penupv,penupol,pepol
       common /pcm_fdc/ qfree,qvol,fdc(MCHS),fs,feps,rcolv,rcol,rcolt
       common /pcm_averages/ spcmsum,spcmcum,spcmcm2 
       common /pcm_averages/ vpcmsum,vpcmcum,vpcmcm2,qopcm_sum,qopcm_cum,qopcm_cm2
