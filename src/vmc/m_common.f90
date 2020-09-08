32181a4a65c0e23a1051ac1f933f7895206eee6e
NicoRenaud <nicolas.gm.renaud@gmail.com>
2020-09-08 13:39:46 +0200

    create mpi mod

--- src/vmc/m_common.f90
+++ src/vmc/m_common.f90
@@ -1902,6 +1902,14 @@ end module forcewt
    save
  end module mmpol_pot
 
+ module mpi_qmc
+  !> Arguments NPROCX
+  integer, parameter :: NPROCX=1024
+  private
+  public :: NPROCX
+  save
+ end module mpi_qmc
+  
  module mpiconf
    !> Arguments: idtask, nproc, wid
     integer  :: idtask
