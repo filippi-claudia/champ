db9f0992f6f9657152a2a94590dc6cf0bdba2377
NicoRenaud <nicolas.gm.renaud@gmail.com>
2020-09-28 22:03:33 +0200

    inverted order ree and rvec_ee

--- src/vmc/hpsi.f
+++ src/vmc/hpsi.f
@@ -43,7 +43,7 @@ c Calculates energy
       ! common /distance/ rshift(3,MELEC,MCENT),rvec_en(3,MELEC,MCENT),r_en(MELEC,MCENT),rvec_ee(3,MMAT_DIM2),r_ee(MMAT_DIM2)
       ! common /distance/ rshift(3,MELEC,MCENT), rvec_ee(3,MMAT_DIM2),r_ee(MMAT_DIM2)
       ! common /distance/ rvec_en(3,MELEC,MCENT),r_en(MELEC,MCENT)
-      common /distance/ rvec_ee(3, MMAT_DIM2), r_ee(MMAT_DIM2)
+      common /distance/ r_ee(MMAT_DIM2), rvec_ee(3, MMAT_DIM2)
 
 
       dimension coord(3,*),psid(*),energy(*)
