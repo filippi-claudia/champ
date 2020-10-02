45c287a448d5cb6e5109276db5da3f1bc860eba9
NicoRenaud <nicolas.gm.renaud@gmail.com>
2020-10-02 16:22:36 +0200

    compare output files for the test

--- src/input/p2nmcheck.f
+++ src/input/p2nmcheck.f
@@ -5,21 +5,26 @@ C this file is auto generated, do not edit
       character lists(21)*(12)
       character vars(153)*(16)
       dimension iaptr(21),ieptr(21)
-      data lists/'mstates','strech','startend','periodic','pseudo',
-     $ 'vmc','properties','jastrow','optgeo','qmmm','electrons',
-     $ 'gradients','3dgrid','ci','optwf','blocking_vmc',
-     $ 'blocking_dmc','atoms','forces','dmc','general'/
-      data vars/'iguiding','iefficiency','alfstr','idump','irstar',
-     $ 'isite','icharged_atom','norb','npoly','np','cutg','cutg_sim',
-     $ 'cutg_big','cutg_sim_big','nloc','nquad','imetro','deltar',
-     $ 'deltat','delta','fbias','node_cutoff','enode_cutoff','sample',
-     $ 'print','ianalyt_lap','ijas','isc','nspin1','nspin2','ifock',
-     $ 'iforce_analy','iuse_zmat','alfgeo','izvzb','iroot_geo',
-     $ 'iqmmm','nelec','nup','ngradnts','igrdtype','delgrdxyz',
-     $ 'delgrdbl','delgrdba','delgrdda','stepx','stepy','stepz','x0',
-     $ 'y0','z0','xn','yn','zn','iciprt','ioptwf','idl_flag',
-     $ 'ilbfgs_flag','ilbfgs_m','method','nopt_iter','ioptjas',
-     $ 'ioptorb','ioptci','multiple_adiag','add_diag',
+      data lists/'dmc','blocking_vmc','blocking_dmc','ci','startend',
+     $ 'properties','mstates','vmc','pseudo','jastrow','forces',
+     $ 'periodic','atoms','optgeo','strech','electrons','3dgrid',
+     $ 'qmmm','optwf','general','gradients'/
+      data vars/'idmc','tau','etrial','nfprod','ipq','itau_eff',
+     $ 'iacc_rej','icross','icuspg','idiv_v','icut_br','icut_e',
+     $ 'icasula','node_cutoff','enode_cutoff','ibranch_elec',
+     $ 'icircular','idrifdifgfunc','mode_dmc','nstep','nblk','nblkeq',
+     $ 'nconf_new','nconf','nstep','nblk','nblkeq','nconf_new',
+     $ 'nconf','iciprt','idump','irstar','isite','icharged_atom',
+     $ 'sample','print','iguiding','iefficiency','imetro','deltar',
+     $ 'deltat','delta','fbias','node_cutoff','enode_cutoff','nloc',
+     $ 'nquad','ianalyt_lap','ijas','isc','nspin1','nspin2','ifock',
+     $ 'istrech','alfstr','nwprod','itausec','norb','npoly','np',
+     $ 'cutg','cutg_sim','cutg_big','cutg_sim_big','nctype','natom',
+     $ 'addghostype','nghostcent','iforce_analy','iuse_zmat','alfgeo',
+     $ 'izvzb','iroot_geo','alfstr','nelec','nup','stepx','stepy',
+     $ 'stepz','x0','y0','z0','xn','yn','zn','iqmmm','ioptwf',
+     $ 'idl_flag','ilbfgs_flag','ilbfgs_m','method','nopt_iter',
+     $ 'ioptjas','ioptorb','ioptci','multiple_adiag','add_diag',
      $ 'ngrad_jas_blocks','nblk_max','nblk_ci','dl_alg','iorbprt',
      $ 'isample_cmat','istddev','limit_cmat','e_shift','save_blocks',
      $ 'force_blocks','iorbsample','iuse_trafo','iuse_orbeigv',
@@ -27,19 +32,14 @@ C this file is auto generated, do not edit
      $ 'energy_tol','sr_tau','sr_eps','sr_adiag','micro_iter_sr',
      $ 'dl_mom','lin_eps','lin_adiag','lin_nvec','lin_nvecx',
      $ 'lin_jdav','func_omega','omega','n_omegaf','n_omegat',
-     $ 'sr_rescale','nstep','nblk','nblkeq','nconf_new','nconf',
-     $ 'nstep','nblk','nblkeq','nconf_new','nconf','nctype','natom',
-     $ 'addghostype','nghostcent','istrech','alfstr','nwprod',
-     $ 'itausec','idmc','tau','etrial','nfprod','ipq','itau_eff',
-     $ 'iacc_rej','icross','icuspg','idiv_v','icut_br','icut_e',
-     $ 'icasula','node_cutoff','enode_cutoff','ibranch_elec',
-     $ 'icircular','idrifdifgfunc','mode_dmc','title','unit','mass',
-     $ 'iperiodic','ibasis','nforce','nwftype','seed','ipr','pool',
-     $ 'basis','pseudopot','i3dsplorb','i3dlagorb','scalecoef'/
-      data iaptr/1,3,4,8,15,17,24,26,32,37,38,40,46,55,56,102,107,112,
-     $ 116,120,139/
-      data ieptr/2,3,7,14,16,23,25,31,36,37,39,45,54,55,101,106,111,
-     $ 115,119,138,153/
+     $ 'sr_rescale','title','unit','mass','iperiodic','ibasis',
+     $ 'nforce','nwftype','seed','ipr','pool','basis','pseudopot',
+     $ 'i3dsplorb','i3dlagorb','scalecoef','ngradnts','igrdtype',
+     $ 'delgrdxyz','delgrdbl','delgrdba','delgrdda'/
+      data iaptr/1,20,25,30,31,35,37,39,46,48,54,58,65,69,74,75,77,86,
+     $ 87,133,148/
+      data ieptr/19,24,29,30,34,36,38,45,47,53,57,64,68,73,74,76,85,
+     $ 86,132,147,153/
       nlist=21
       ierr=0
       do i=1,nlist
---
+++ tools/compare_output_file.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import itertools
+import argparse
+
+
+def predicate(line):
+    if 'END' in line:
+        return False
+    return True
+
+
+def read_file(filename):
+    f = open(filename)
+    # data = itertools.ifilter(predicate, f)
+    data = f.readlines()
+    f.close()
+    return data
+
+
+def compare_file(data1, data2):
+    for l1, l2 in zip(data1, data2):
+        if 'END' in l1:
+            continue
+        if l1 != l2:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="compare two files")
+    parser.add_argument("filename1", help="name of the first file")
+    parser.add_argument("filename2", help="name of the second file")
+    args = parser.parse_args()
+
+    data1 = read_file(args.filename1)
+    data2 = read_file(args.filename2)
+
+    assert(compare_file(data1, data2))
+    # print(all(x == y) for x, y in itertools.izip(data1, data2))
