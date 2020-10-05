09e7c3c676c6130da2e0c2a13fe98a05a94ac53a
NicoRenaud <nicolas.gm.renaud@gmail.com>
2020-10-05 16:58:45 +0200

    compare values in the test

--- tools/compare_output_file.py
+++ tools/compare_output_file.py
@@ -29,15 +29,24 @@ def compare_file(data1, data2):
     return True
 
 
+def compare_values(data, keyword, ref_values, kw_col=0, val_col=1):
+    for l in data:
+        if data[kw_col] == keyword:
+            assert(float(data[val_col]) == ref_values)
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="compare two files")
     parser.add_argument("filename1", help="name of the first file")
     parser.add_argument("filename2", help="name of the second file")
+    parser.add_argument("-kw", "--keyword", default=None,
+                        help="name of the variable to test")
+    parser.add_argument(
+        "-v", "--values", default=None, help="reference value")
     args = parser.parse_args()
 
     data1 = read_file(args.filename1)
     data2 = read_file(args.filename2)
 
     assert(compare_file(data1, data2))
-    # print(all(x == y) for x, y in itertools.izip(data1, data2))
