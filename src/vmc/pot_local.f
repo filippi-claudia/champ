585f98cb65c1395c07442c4755a4452e1220254e
NicoRenaud <nicolas.gm.renaud@gmail.com>
2020-10-05 20:51:03 +0200

    Revert "switch order to create error"
    This reverts commit 2d10d7857c5bf613e91e51309bac31676a0efba0.

--- src/vmc/pot_local.f
+++ src/vmc/pot_local.f
@@ -15,7 +15,7 @@
       ! common /distance/ rshift(3,MELEC,MCENT),rvec_en(3,MELEC,MCENT),r_en(MELEC,MCENT),rvec_ee(3,MMAT_DIM2),r_ee(MMAT_DIM2)
       ! common /distance/ rshift(3,MELEC,MCENT), rvec_ee(3,MMAT_DIM2), r_ee(MMAT_DIM2)
       ! common /distance/ rvec_en(3,MELEC,MCENT),r_en(MELEC,MCENT)
-      common /distance/  r_ee(MMAT_DIM2), rvec_ee(3, MMAT_DIM2)
+      common /distance/  rvec_ee(3, MMAT_DIM2), r_ee(MMAT_DIM2)
 c  pe from nucleus-nucleus repulsion
       pe=pecent
       pe_ee=0.d0
