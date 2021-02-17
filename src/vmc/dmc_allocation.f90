c8fd34e600a96408c8909907d53396e8b691cec2
lopeztarifa <pablo.lopez.tarifa@gmail.com>
2021-02-17 15:57:14 +0100

    Add forgotten dmc_allocation.f90 file.

---
+++ src/vmc/dmc_allocation.f90
@@ -0,0 +1,21 @@
+!> Subroutines to allocate/deallocate memory in DMC-related
+!> variables:
+subroutine allocate_m_dmc()
+  use age, only: allocate_iage
+  use contrldmc, only: allocate_contrldmc 
+  use config, only: allocate_config_dmc 
+  !> Allocate all the dmc_mod arrays:
+  call allocate_iage()
+  call allocate_contrldmc()
+  call allocate_config_dmc()
+end subroutine allocate_m_dmc
+
+subroutine deallocate_m_dmc()
+  use age, only: deallocate_iage
+  use contrldmc, only: deallocate_contrldmc 
+  use config, only: deallocate_config_dmc 
+  !> Deallocate all the dmc_mod arrays:
+  call deallocate_iage()
+  call deallocate_contrldmc()
+  call deallocate_config_dmc()
+end subroutine deallocate_m_dmc
