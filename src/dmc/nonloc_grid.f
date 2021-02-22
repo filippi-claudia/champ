7d6ebe5e5a6bb5290ae0cf1faa4803213bb5d0da
lopeztarifa <pablo.lopez.tarifa@gmail.com>
2021-02-22 16:26:56 +0100

    Delete clbock dmc /optwf_corsam/.

--- src/dmc/nonloc_grid.f
+++ src/dmc/nonloc_grid.f
@@ -27,8 +27,6 @@
      &,d2ijo(MELEC,MELEC),d2jo,fsumo,fjo(3,MELEC)
 
       common /distance/ rshift(3,MELEC,MCENT),rvec_en(3,MELEC,MCENT),r_en(MELEC,MCENT),rvec_ee(3,MMAT_DIM2),r_ee(MMAT_DIM2)
-      ! common /distance/ rshift(3,MELEC,MCENT), rvec_ee(3,MMAT_DIM2),r_ee(MMAT_DIM2)
-
 
 
 
