2b90883e613e96193cf191580300e0a5ef3df680
lopeztarifa <pablo.lopez.tarifa@gmail.com>
2021-03-08 15:27:48 +0100

    Clean more variables in dmc.

--- src/dmc/dumper.f
+++ src/dmc/dumper.f
@@ -2,75 +2,54 @@
 c MPI version created by Claudia Filippi starting from serial version
 c routine to pick up and dump everything needed to restart
 c job where it left off
-      use vmc_mod, only: MELEC, MORB, MBASIS, MDET, MCENT, MCTYPE, MCTYP3X,
-     &NSPLIN, nrad, MORDJ, MORDJ1, MMAT_DIM, MMAT_DIM2, MMAT_DIM20,
-     &radmax, delri, NEQSX, MTERMS, MCENT3, NCOEF, MEXCIT
-      use dmc_mod, only: MWALK, MFPROD, MFPRD1, MPATH
-      use basis, only: zex, betaq, n1s, n2s, n2p, n3s, n3p, n3dzr, n3dx2, n3dxy, n3dxz, n3dyz,
-     & n4s, n4p, n4fxxx, n4fyyy, n4fzzz, n4fxxy, n4fxxz, n4fyyx, n4fyyz,
-     & n4fzzx, n4fzzy, n4fxyz, nsa, npa, ndzra, ndz2a, ndxya, ndxza, ndyza, ndx2a
-      use const, only: delta, deltai, etrial, fbias, hb, imetro, ipr, nelec, pi
+      use vmc_mod, only: MELEC
+      use vmc_mod, only: nrad
+      use dmc_mod, only: MWALK
+      use basis, only: zex, n1s, n2s, n2p, n3s, n3p, n3dzr, n3dx2, n3dxy, n3dxz, n3dyz
+      use basis, only: n4s, n4p
+      use basis, only: nsa, npa, ndzra, ndxya, ndxza, ndyza, ndx2a
+      use const, only: hb, nelec
       use forcest, only: fgcm2, fgcum
-      use forcepar, only: deltot, istrech, nforce
+      use forcepar, only: nforce
       use age, only: iage, ioldest, ioldestmx
-      use contrldmc, only: iacc_rej, icross, icuspg, icut_br, icut_e, idiv_v, idmc, ipq,
-     &itau_eff, nfprod, rttau, tau, taueff, tautot
+      use contrldmc, only: idmc
+      use contrldmc, only: nfprod, rttau, tau
       use atom, only: cent, iwctype, ncent, nctype, pecent, znuc
-
       use estcum, only: iblk, ipass
-      use config, only: d2o, peo_dmc, psido_dmc, psijo_dmc, vold_dmc, xold_dmc
-
+      use config, only: xold_dmc
       use stats, only: acc, dfus2ac, dfus2un, dr2ac, dr2un, nacc, nbrnch, nodecr, trymove
-
-      use estsum, only: efsum, efsum1, egsum, egsum1, ei1sum, ei2sum, ei3sum, esum1_dmc, esum_dmc,
-     &pesum_dmc, r2sum, risum, tausum, tjfsum_dmc, tpbsum_dmc, w_acc_sum, w_acc_sum1, wdsum,
-     &wdsum1, wfsum, wfsum1, wg_acc_sum, wg_acc_sum1, wgdsum, wgsum, wgsum1, wsum1, wsum_dmc
-      use estcum, only: ecum1_dmc, ecum_dmc, efcum, efcum1, egcum, egcum1, ei1cum, ei2cum,
-     &ei3cum, pecum_dmc, r2cum_dmc, ricum, taucum, tjfcum_dmc, tpbcum_dmc, w_acc_cum, w_acc_cum1,
-     &wcum1, wcum_dmc, wdcum, wdcum1, wfcum, wfcum1, wg_acc_cum, wg_acc_cum1, wgcum, wgcum1,
-     &wgdcum
-      use force_dmc, only: itausec, nwprod
-      use est2cm, only: ecm21_dmc, ecm2_dmc, efcm2, efcm21, egcm2, egcm21, ei1cm2, ei2cm2,
-     &ei3cm2, pecm2_dmc, r2cm2_dmc, ricm2, tjfcm_dmc, tpbcm2_dmc, wcm2, wcm21, wdcm2, wdcm21,
-     &wfcm2, wfcm21, wgcm2, wgcm21, wgdcm2
-      use derivest, only: derivcm2, derivcum, derivsum, derivtotave_num_old
-
-      use step, only: ekin, ekin2, rprob, suc, trunfb, try
+      use estcum, only: ecum1_dmc, ecum_dmc, efcum, efcum1, egcum, egcum1, ei1cum, ei2cum
+      use estcum, only: ei3cum, pecum_dmc, r2cum_dmc, ricum, taucum, tjfcum_dmc, tpbcum_dmc
+      use estcum, only: wcum1, wcum_dmc, wdcum, wdcum1, wfcum, wfcum1, wgcum, wgcum1
+      use estcum, only: wgdcum
+      use force_dmc, only: nwprod
+      use est2cm, only: ecm21_dmc, ecm2_dmc, efcm2, efcm21, egcm2, egcm21, ei1cm2, ei2cm2
+      use est2cm, only: ei3cm2, pecm2_dmc, r2cm2_dmc, ricm2, tjfcm_dmc, tpbcm2_dmc, wcm2, wcm21, wdcm2, wdcm21
+      use est2cm, only: wfcm2, wfcm21, wgcm2, wgcm21, wgdcm2
+      use derivest, only: derivcm2, derivcum, derivtotave_num_old
+      use step, only: rprob
       use mpiconf, only: idtask, nproc, wid, NPROCX
-
       use denupdn, only: rprobdn, rprobup
       use contr3, only: mode
       use mpiblk, only: iblk_proc
-      use force_mod, only: MFORCE, MFORCE_WT_PRD, MWF
-      use mstates_mod, only: MSTATES, MDETCSFX
-      use pseudo_mod, only: MPS_L, MPS_QUAD, MPS_GRID, MGAUSS
-
-      use qua, only: nquad, wq, xq, xq0, yq, yq0, zq, zq0
-
-      use branch, only: eest, eigv, eold, ff, fprod, nwalk, pwt, wdsumo, wgdsumo, wt, wtgen,
-     &wthist
-      use casula, only: i_vpsp, icasula, t_vpsp
-      use jacobsave, only: ajacob, ajacold
-      use pseudo, only: lpot, nloc, vps, vpso
-
+      use qua, only: nquad, wq, xq, yq, zq
+      use branch, only: eest, eigv, ff, fprod, nwalk, pwt, wdsumo, wgdsumo, wt, wtgen
+      use branch, only: wthist
+      use jacobsave, only: ajacob
+      use pseudo, only: nloc
       use dets, only: cdet, ndet
       use elec, only: ndn, nup
       use coefs, only: coef, nbasis, norb
       use ghostatom, only: newghostype, nghostcent
-      use jaspar, only: is, nspin1, nspin2, sspin, sspinn
       use jaspar1, only: cjas1, cjas2
-      use velratio, only: fratio, xdrifted
-      use contrl, only: idump, irstar, isite, nblk, nblkeq, nconf, nconf_new, nstep
+      use velratio, only: fratio
+      use contrl, only: nconf
       use mpi
 
       implicit real*8(a-h,o-z)
 
-
       parameter (zero=0.d0,one=1.d0)
       parameter (small=1.e-6)
-
-
-
 
       dimension irn(4,0:NPROCX),istatus(MPI_STATUS_SIZE)
       dimension irn_tmp(4,0:NPROCX)
