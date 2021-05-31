f8b6db8768b485ad4bdf5fc95577793f5afffddc
lopeztarifa <pablo.lopez.tarifa@gmail.com>
2021-05-31 10:39:16 +0200

    Add implicit none to optwf_vmc.f

--- src/vmc/optwf_vmc.f
+++ src/vmc/optwf_vmc.f
@@ -1,11 +1,15 @@
       subroutine qmc
 
+      implicit none
+
       call vmc
 
       return
       end
 c---------------------------------------------------
       subroutine reset_configs_start
+
+      implicit none
 
       call mc_configs_start
 
