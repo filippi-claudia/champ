f64dafba1e5a801607d7930a743b2588782d48e3
Ravindra Shinde <neelravi@gmail.com>
2021-06-02 15:42:18 +0200

    triggering the new self-hosted test for vmc optimization

--- tests/CI_test/new-parser-butadiene-vmc-only/compile_and_run.sh
+++ tests/CI_test/new-parser-butadiene-vmc-only/compile_and_run.sh
@@ -1,3 +1,2 @@
 cp -v mc_configs_start_save mc_configs_start
-mpirun -np 1  /home/ravindra/softwares/neelravi-champ-modern/bin/vmc.mov1 -i test-champ.inp  -o test.out  -e error > screen 
-#mpirun -np 1  /home/ravindra/softwares/neelravi-champ-modern/bin/dmc.mov1 -i test-champ.inp  -o test.out  -e error 
+mpirun -np 1  /home/ravindra/softwares/neelravi-champ-modern/bin/vmc.mov1 -i vmc_only.inp  -o vmc_only.out  -e error_vmc_only > screen 
