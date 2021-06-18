5c4e5acaa0d19606deb5c7c82a365d677ffce8e6
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2021-06-18 17:28:19 +0200

    adding implicit none to rannyu.f

--- src/vmc/rannyu.f
+++ src/vmc/rannyu.f
@@ -3,7 +3,10 @@ c NYU linear congruential random number generator.
 c Uses 48 bits, rather than the usual 32 bits.
 
       use rnyucm, only: ll
-      implicit real*8(a-h,o-z)
+      implicit none
+
+      integer :: i
+
 
       integer iseed(4)
          do 10 i=1,4
@@ -39,11 +42,18 @@ c-----------------------------------------------------------------------
       function rannyu(idum)
       use rnyucm, only: ll, mm
 
-      implicit real*8(a-h,o-z)
+      use precision_kinds, only: dp
+      implicit none
+
+      integer, intent(in) :: idum
+      integer :: i1, i2, i3, i4
+      integer, parameter :: itwo12 = 4096
+
+      real(dp), parameter :: two12i = 2.44140625d-4
+      real(dp) :: rannyu
 
 c     On bat it is more efficient to not precompute 2**12 and 2**-12
 c     Therefore use original verion of code instead of this one.
-      parameter(itwo12=4096,two12i=2.44140625d-4)
       i1=ll(1)*mm(4)+ll(2)*mm(3)+ll(3)*mm(2)+ll(4)*mm(1)
       i2=ll(2)*mm(4)+ll(3)*mm(3)+ll(4)*mm(2)
       i3=ll(3)*mm(4)+ll(4)*mm(3)
@@ -64,7 +74,10 @@ c-----------------------------------------------------------------------
 
       subroutine savern(iseed)
       use rnyucm, only: ll
-      implicit real*8(a-h,o-z)
+      implicit none
+
+      integer :: i
+
 
       integer iseed(4)
       do 10 i=1,4
