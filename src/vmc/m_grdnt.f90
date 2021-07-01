7d41423203415d1a65b51344b9864b44153a2b4c
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2021-07-01 14:17:53 +0200

    m_grid.f90

--- src/vmc/m_grdnt.f90
+++ src/vmc/m_grdnt.f90
@@ -1,7 +1,6 @@
 module grdnthes
      !> Arguments: hessian_zmat
      use precision_kinds, only: dp
-     use vmc_mod, only: MCENT
 
      implicit none
 
@@ -14,8 +13,6 @@ module grdnthes
  contains
      subroutine allocate_grdnthes()
         use atom, only: ncent_tot
-         use precision_kinds, only: dp
-         use vmc_mod, only: MCENT
          if (.not. allocated(hessian_zmat)) allocate (hessian_zmat(3, ncent_tot))
      end subroutine allocate_grdnthes
 
@@ -28,7 +25,6 @@ module grdnthes
  module grdntsmv
      !> Arguments: igrdaidx, igrdcidx, igrdmv
      use force_mod, only: MFORCE
-     use vmc_mod, only: MCENT
 
      implicit none
 
@@ -44,7 +40,6 @@ module grdnthes
      subroutine allocate_grdntsmv()
         use atom, only: ncent_tot
          use force_mod, only: MFORCE
-         use vmc_mod, only: MCENT
          if (.not. allocated(igrdaidx)) allocate (igrdaidx(MFORCE))
          if (.not. allocated(igrdcidx)) allocate (igrdcidx(MFORCE))
          if (.not. allocated(igrdmv)) allocate (igrdmv(3, ncent_tot))
