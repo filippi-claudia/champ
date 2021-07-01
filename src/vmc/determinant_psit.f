745392b98f56c6dd983fdd603c5031d186609f97
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2021-07-01 11:49:49 +0200

    cleaned dets

--- src/vmc/determinant_psit.f
+++ src/vmc/determinant_psit.f
@@ -1,11 +1,5 @@
       subroutine determinant_psit(determ,istate)
 
-      use force_mod, only: MFORCE, MFORCE_WT_PRD, MWF
-      use vmc_mod, only: MELEC, MORB, MBASIS, MDET, MCENT, MCTYPE, MCTYP3X
-      use vmc_mod, only: NSPLIN, nrad, MORDJ, MORDJ1, MMAT_DIM, MMAT_DIM2, MMAT_DIM20
-      use vmc_mod, only: radmax, delri
-      use vmc_mod, only: NEQSX, MTERMS
-      use vmc_mod, only: MCENT3, NCOEF, MEXCIT
       use dets, only: cdet, ndet
       use wfsec, only: iwf
 
