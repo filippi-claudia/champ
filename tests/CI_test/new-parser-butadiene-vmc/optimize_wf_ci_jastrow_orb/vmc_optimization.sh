ad1cccc05f525cf13a302e22a96e040becd4894b
Ravindra Shinde <neelravi@gmail.com>
2021-07-02 12:01:32 +0200

    removing unnecessary files; preparing for the merge

--- tests/CI_test/new-parser-butadiene-vmc/optimize_wf_ci_jastrow_orb/vmc_optimization.sh
+++ tests/CI_test/new-parser-butadiene-vmc/optimize_wf_ci_jastrow_orb/vmc_optimization.sh
@@ -1,2 +1,3 @@
 cp -v mc_configs_start_save mc_configs_start
-mpirun -np 2  /home/ravindra/softwares/neelravi-champ-modern/bin/vmc.mov1 -i vmc_optimization.inp  -o vmc_optimization.out  -e error_vmc_optimization
+mpirun -np 1  /home/ravindra/softwares/neelravi-champ-modern/bin/vmc.mov1 -i vmc_optimization.inp  -o vmc_optimization_single.out  -e error_vmc_optimization
+mpirun -np 2  /home/ravindra/softwares/neelravi-champ-modern/bin/vmc.mov1 -i vmc_optimization.inp  -o vmc_optimization_double.out  -e error_vmc_optimization
--- tests/CI_test/new-parser-psb3/run_vmcopt.sh
+++
@@ -1,2 +0,0 @@
-#cp -v mc_configs_start_save mc_configs_start
-mpirun -np 1 /home/ravindra/softwares/neelravi-champ-modern/bin/vmc.mov1 -i revised_sr_n.inp -o revised_sr_n_double.out -e error 
