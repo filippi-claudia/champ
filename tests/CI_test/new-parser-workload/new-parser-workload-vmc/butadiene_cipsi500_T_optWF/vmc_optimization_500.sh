0c943415427115a26fa906bd0b3d4fb329769e49
Ravindra Shinde <neelravi@gmail.com>
2021-08-12 16:00:20 +0200

    scripts to run local tests added; workflow file changed to address the intel issue

--- tests/CI_test/new-parser-workload/new-parser-workload-vmc/butadiene_cipsi500_T_optWF/vmc_optimization_500.sh
+++ tests/CI_test/new-parser-workload/new-parser-workload-vmc/butadiene_cipsi500_T_optWF/vmc_optimization_500.sh
@@ -1,1 +1,2 @@
+mpirun -np 1  /home/ravindra/softwares/neelravi-champ-modern/bin/vmc.mov1 -i vmc_optimization_500.inp  -o vmc_optimization_500.out  -e error_vmc_optimization_500
 mpirun -np 2  /home/ravindra/softwares/neelravi-champ-modern/bin/vmc.mov1 -i vmc_optimization_500.inp  -o vmc_optimization_500.out  -e error_vmc_optimization_500
