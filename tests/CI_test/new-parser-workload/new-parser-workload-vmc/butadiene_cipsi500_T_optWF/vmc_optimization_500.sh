1ef83485366c09c272414c511f8f5014e6efaf5f
Ravindra Shinde <neelravi@gmail.com>
2021-08-19 14:08:22 +0200

    minor update

--- tests/CI_test/new-parser-workload/new-parser-workload-vmc/butadiene_cipsi500_T_optWF/vmc_optimization_500.sh
+++ tests/CI_test/new-parser-workload/new-parser-workload-vmc/butadiene_cipsi500_T_optWF/vmc_optimization_500.sh
@@ -1,2 +1,2 @@
-mpirun -np 1  /home/ravindra/softwares/neelravi-champ-modern/bin/vmc.mov1 -i vmc_optimization_500.inp  -o vmc_optimization_500.out  -e error_vmc_optimization_500
-mpirun -np 2  /home/ravindra/softwares/neelravi-champ-modern/bin/vmc.mov1 -i vmc_optimization_500.inp  -o vmc_optimization_500.out  -e error_vmc_optimization_500
+mpirun -np 1  ../../../../../bin/vmc.mov1 -i vmc_optimization_500.inp  -o vmc_optimization_500_single.out  -e error_vmc_optimization_500
+mpirun -np 2  ../../../../../bin/vmc.mov1 -i vmc_optimization_500.inp  -o vmc_optimization_500_double.out  -e error_vmc_optimization_500
