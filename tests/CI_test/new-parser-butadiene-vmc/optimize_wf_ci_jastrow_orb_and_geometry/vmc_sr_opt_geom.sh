70912408a319324e5b1fed2add5625967f1ca120
Ravindra Shinde <neelravi@gmail.com>
2021-08-19 11:14:16 +0200

    starting without reference configurations

--- tests/CI_test/new-parser-butadiene-vmc/optimize_wf_ci_jastrow_orb_and_geometry/vmc_sr_opt_geom.sh
+++ tests/CI_test/new-parser-butadiene-vmc/optimize_wf_ci_jastrow_orb_and_geometry/vmc_sr_opt_geom.sh
@@ -1,4 +1,2 @@
-cp -v mc_configs_start_save mc_configs_start
 mpirun -np 1  ../../../../bin/vmc.mov1 -i vmc_sr_opt_geom.inp  -o vmc_sr_opt_geom_single.out  -e error_vmc_optimization
-cp -v mc_configs_start_save mc_configs_start
 mpirun -np 2  ../../../../bin/vmc.mov1 -i vmc_sr_opt_geom.inp  -o vmc_sr_opt_geom_double.out  -e error_vmc_optimization
