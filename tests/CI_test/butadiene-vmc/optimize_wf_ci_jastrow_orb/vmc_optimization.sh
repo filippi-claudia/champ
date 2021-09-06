d9d5511475d85622721eb2732882349970af0f9a
Ravindra Shinde <neelravi@gmail.com>
2021-09-06 10:54:19 +0200

    step 1 : test folders being renamed; tests made shorter

---
+++ tests/CI_test/butadiene-vmc/optimize_wf_ci_jastrow_orb/vmc_optimization.sh
@@ -0,0 +1,2 @@
+mpirun -np 1  ../../../../bin/vmc.mov1 -i vmc_optimization.inp  -o vmc_optimization_single.out  
+mpirun -np 2  ../../../../bin/vmc.mov1 -i vmc_optimization.inp  -o vmc_optimization_double.out  
