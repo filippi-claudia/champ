06c5ff1dad293869049314036a7c948113e406b0
Ravindra Shinde <neelravi@gmail.com>
2021-09-08 15:49:08 +0200

    testsuite :: extra tests added ; cleanup of redundant tests;

--- tests/CI_test/VMC-Butadiene-0.5k-5k-15k-dets/butadiene_ras1022_Q_optWF+geo/vmc_optimization_45000.sh
+++
@@ -1,2 +0,0 @@
-mpirun -np 1  ../../../../bin/vmc.mov1 -i vmc_optimization_45000.inp  -o vmc_optimization_45000.out -e error 
-mpirun -np 2  ../../../../bin/vmc.mov1 -i vmc_optimization_45000.inp  -o vmc_optimization_45000.out -e error
