73786fb486cbcd41c0be9e58193ef896f5ba499d
Victor Azizi <victor@lipsum.eu>
2022-09-09 15:53:59 +0200

    added skeleton for jastrow test

--- src/vmc/main.f90
+++ src/vmc/main.f90
@@ -11,15 +11,16 @@
 ! URL           : https://github.com/filippi-claudia/champ
 !---------------------------------------------------------------------------
 
-program main
+module main_mod
 
-    use mpi
+contains
+
+subroutine initialize_main
+    use mpi_f08
     use mpiconf, only: idtask, nproc
     use mpiconf, only: mpiconf_init
     use contr3, only: init_control_mode
     use contrl_file, only: init_logfile, init_procfile, close_files, initialize
-    use allocation_mod, only: deallocate_vmc
-    use optwf_mod, only: optwf
     use mpiconf, only: wid      ! logical :: true only for mpirank=0
     use precision_kinds,    only: dp
     use contrl_file,    only: ounit
@@ -59,8 +60,13 @@ program main
     call MPI_BARRIER(MPI_Comm_World, ierr)
     call elapsed_time("MPI Barrier before optwf : ")
 
-    ! ! run the the optimization
-    call optwf()
+end subroutine
+
+subroutine finalize_main()
+    use mpi_f08
+    use allocation_mod, only: deallocate_vmc
+    use contrl_file,    only: ounit
+    use mpitimer,    only: time, time_start, time_final
 
     ! call close_files()
     time_final = time()
@@ -69,5 +75,22 @@ program main
 
     call mpi_finalize(ierr)
     call deallocate_vmc()
+end subroutine
 
+end module
+
+
+program main
+    use main_mod, only: initialize_main, finalize_main
+    use optwf_mod, only: optwf
+
+    implicit None
+    integer :: ierr
+
+    call initialize_main()
+
+    ! ! run the the optimization
+    call optwf()
+
+    call finalize_main()
 end
---
+++ tests/unit_test/env.f90
@@ -0,0 +1,5 @@
+module env_mod
+
+  
+
+end module
