1e090b29d4bf83b309cd2facc5a3523b9bed1a7b
Ravindra Shinde <neelravi@gmail.com>
2022-09-13 16:33:53 +0200

    test vmc butadiene 1522 dets converted to v3 format

---
+++ tests/CI_test/VMC-Butadiene-ci1010_SDT_pVTZ-1522-dets/conversion/script_conversion.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python3 ../../../../tools/convert_lcao_to_trexio.py \
+			--lcao champ_v2_gamess_ci1010_SDT_pVTZ_orbitals.lcao \
+			--geom butadiene.xyz \
+			--bfinfo BFD-T.bfinfo  
+
