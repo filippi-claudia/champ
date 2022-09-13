047fd13cefede4ff209414374a12a3d343f2197f
Ravindra Shinde <neelravi@gmail.com>
2022-09-13 11:59:37 +0200

    test vmc butadiene optall converted to v3 format

---
+++ tests/CI_test/VMC-Butadiene/ci44_pVQZ_20_dets_12_csf_optall/conversion/script_conversion.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python3 ../../../../../tools/convert_lcao_to_trexio.py \
+			--lcao champ_v2_gamess_ci44_pVQZ_cart.lcao \
+			--geom butadiene_champ_v2_gamess_ci44_pVQZ_1_geom.xyz \
+			--bfinfo champ_v2_gamess_ci44_pVQZ.bfinfo
+
