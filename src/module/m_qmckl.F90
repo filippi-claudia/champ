670612b803852b3c3c9b3203e67f7d10fea6f067
Edgar Landinez <josuelandinez@gmail.com>
2022-11-14 16:32:40 +0100

    Cmake in vmc and dmc

---
+++ src/module/m_qmckl.F90
@@ -0,0 +1,21 @@
+#ifdef QMCKL_FOUND
+#include "qmckl_f.F90"
+#endif
+
+module qmckl_data
+#ifdef QMCKL_FOUND
+
+  use qmckl
+  integer(qmckl_context), public :: qmckl_ctx
+  logical, public :: use_qmckl = .True.
+
+#else
+
+  integer, public :: qmckl_ctx = 0
+  integer, public :: QMCKL_SUCCESS = 0
+  logical, public :: use_qmckl = .False.
+
+#endif
+end module
+
+
---
+++ src/module/m_trexio.F90
@@ -0,0 +1,4 @@
+#ifdef TREXIO_FOUND
+#include "trexio_f.f90"
+#endif
+
