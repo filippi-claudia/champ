0d81313582af2a3565e05afe077f0d7f52aa7394
Ravindra Shinde <neelravi@gmail.com>
2022-11-21 15:04:30 +0100

    fixing the reference values

--- tests/CI_test/VMC-TREXIO-CH2O-excited-8724-dets-BFD-aug-cc-pVDZ/VMC_TREXIO_CH2O_excited_8724-dets_BFD-aug-cc-pVDZ_formaldehyde_energy.sh
+++ tests/CI_test/VMC-TREXIO-CH2O-excited-8724-dets-BFD-aug-cc-pVDZ/VMC_TREXIO_CH2O_excited_8724-dets_BFD-aug-cc-pVDZ_formaldehyde_energy.sh
@@ -5,8 +5,8 @@ output="trexio_vmc_COH2_excited_state"
 
 # Multicore test
 N=8
-ReferenceEnergy=-22.1028217
-ReferenceError=0.0066176
+ReferenceEnergy=-22.6248468
+ReferenceError=0.0030813
 mpirun -np $N ../../../bin/vmc.mov1 -i $input -o ${output}_core_${N}.out -e error
 echo "Comparing energy with reference Core=$N           (total E = $ReferenceEnergy +-  $ReferenceError ) "
 ../../../tools/compare_value.py ${output}_core_${N}.out     "total E"  $ReferenceEnergy     $ReferenceError
--- tests/CI_test/VMC-TREXIO-text-hdf5-backend-comparison/VMC_TREXIO_text_hdf5_backend_comparison_butadiene_energy_ci1010_pVTZ.sh
+++ tests/CI_test/VMC-TREXIO-text-hdf5-backend-comparison/VMC_TREXIO_text_hdf5_backend_comparison_butadiene_energy_ci1010_pVTZ.sh
@@ -4,8 +4,8 @@ output="vmc_opt_ci1010_pVTZ_1522_hdf5"
 
 # unicore test
 N=1
-ReferenceEnergyhdf5=-24.1199689
-ReferenceError=0.0545703
+ReferenceEnergyhdf5=-26.0085168
+ReferenceError=0.0221974
 mpirun -np $N ../../../bin/vmc.mov1 -i ${input} -o ${output}_core_${N}.out -e error
 echo "Comparing energy with reference Core=${N}           (total E = $ReferenceEnergyhdf5 +-  $ReferenceError ) "
 ../../../tools/compare_value.py ${output}_core_${N}.out     "total E"  $ReferenceEnergyhdf5     $ReferenceError
@@ -17,8 +17,8 @@ output="vmc_opt_ci1010_pVTZ_1522_text"
 
 # unicore test
 N=1
-ReferenceEnergytext=-24.1199689
-ReferenceError=0.0545703
+ReferenceEnergytext=-26.0085168
+ReferenceError=0.0221974
 mpirun -np $N ../../../bin/vmc.mov1 -i ${input} -o ${output}_core_${N}.out -e error
 echo "Comparing energy with reference Core=${N}           (total E = $ReferenceEnergytext +-  $ReferenceError ) "
 ../../../tools/compare_value.py ${output}_core_${N}.out     "total E"  $ReferenceEnergytext     $ReferenceError
