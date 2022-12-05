e6f0d50e1b5a5e95475e42d56f2481105fe63d04
Ravindra Shinde <neelravi@users.noreply.github.com>
2022-12-05 19:52:49 +0100

    Bugfix 4 dwfn trexio (#233)
    * bugfix step 4 read bas num
    
    * dwfn restored in the equivalent subroutine of trexio

--- src/vmc/read_trexio_data.f90
+++ src/vmc/read_trexio_data.f90
@@ -912,6 +912,8 @@ module trexio_read_data
 ! Nonzero basis at the boundary : Do exponential fitting.
                 if(dabs(rmaxwf(irb,ic)-x(nr(ic))).lt.1.0d-10) then
                     call exp_fit(x(nr(ic)-9:nr(ic)),rwf(nr(ic)-9:nr(ic),irb,ic,iwf), 10, ae(1,irb,ic,iwf), ae(2,irb,ic,iwf))
+                    if(ae(2,irb,ic,iwf).lt.0) call fatal_error ('BASIS_READ_NUM: ak<0')
+
                     rmaxwf(irb,ic)=-dlog(cutoff_rmax/ae(1,irb,ic,iwf))/ae(2,irb,ic,iwf)
 
                     write(45,'(a)') 'check the large radius expansion'
@@ -923,11 +925,13 @@ module trexio_read_data
                       temp = ae(1,irb,ic,iwf)*dexp(-ae(2,irb,ic,iwf)*x(nr(ic)-ir))
                       write(45,'(i3,2x,1p4e22.14)') ir,x(nr(ic)-ir),rwf(nr(ic)-ir,irb,ic,iwf), temp
                     enddo
-                    write(45,*) 'dwf1,dwfn',dwf1,dwfn
 
-                    if(ae(2,irb,ic,iwf).lt.0) call fatal_error ('BASIS_READ_NUM: ak<0')
+                    dwfn=-ae(2,irb,ic,iwf)*ae(1,irb,ic,iwf)*dexp(-ae(2,irb,ic,iwf)*x(nr(ic)))
 
+                else
+                    dwfn=0.d0
                 endif
+                write(45,*) 'dwf1,dwfn',dwf1,dwfn
 
                 call spline2(x,rwf(1,irb,ic,iwf),nr(ic),dwf1,dwfn, d2rwf(1,irb,ic,iwf), work)
 
