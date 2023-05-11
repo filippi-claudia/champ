f7c7e246cd6b1e9151f73a6d30fc886783118038
s.s.shepard@utwente.nl <shepard@ccpgate.tnw.utwente.nl>
2023-05-11 15:11:45 +0200

    update reference values for tests 5,7,8 for merging

--- tests/CI_test/VMC-C4H6-ci44_pVQZ_20_dets_12_csf_optall/VMC_C4H6_ci44_pVQZ_20-dets_12-csf_optimization_butadiene.sh
+++ tests/CI_test/VMC-C4H6-ci44_pVQZ_20_dets_12_csf_optall/VMC_C4H6_ci44_pVQZ_20-dets_12-csf_optimization_butadiene.sh
@@ -5,8 +5,8 @@ output="vmc_optall_ci44"
 
 # unicore test
 N=1
-ReferenceEnergy=-26.0763534
-ReferenceError=0.0144316
+ReferenceEnergy=-26.1895437
+ReferenceError=0.0138485
 mpirun -np $N ../../../bin/vmc.mov1 -i $input -o ${output}_core_${N}.out -e error
 echo "Comparing energy with reference Core=$N           (total E = $ReferenceEnergy +-  $ReferenceError ) "
 ../../../tools/compare_value.py ${output}_core_${N}.out     "total E"  $ReferenceEnergy     $ReferenceError
@@ -14,8 +14,8 @@ echo "Comparing energy with reference Core=$N           (total E = $ReferenceEne
 
 # Multicore test
 N=2
-ReferenceEnergy=-26.1864259
-ReferenceError=0.0109200
+ReferenceEnergy=-26.2433962
+ReferenceError=0.0105989
 mpirun -np $N ../../../bin/vmc.mov1 -i $input -o ${output}_core_${N}.out -e error
 echo "Comparing energy with reference Core=$N           (total E = $ReferenceEnergy +-  $ReferenceError ) "
 ../../../tools/compare_value.py ${output}_core_${N}.out     "total E"  $ReferenceEnergy     $ReferenceError
