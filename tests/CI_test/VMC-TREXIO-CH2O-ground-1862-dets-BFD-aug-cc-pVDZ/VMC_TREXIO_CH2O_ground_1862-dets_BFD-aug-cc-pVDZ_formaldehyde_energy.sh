17db737b2205f19e320440cfeaedd1abf3e11249
Ravindra Shinde <neelravi@gmail.com>
2023-11-13 10:26:11 +0100

    Running tests longer for better stats; version number updated; github action cleaned

--- tests/CI_test/VMC-TREXIO-CH2O-ground-1862-dets-BFD-aug-cc-pVDZ/VMC_TREXIO_CH2O_ground_1862-dets_BFD-aug-cc-pVDZ_formaldehyde_energy.sh
+++ tests/CI_test/VMC-TREXIO-CH2O-ground-1862-dets-BFD-aug-cc-pVDZ/VMC_TREXIO_CH2O_ground_1862-dets_BFD-aug-cc-pVDZ_formaldehyde_energy.sh
@@ -5,8 +5,8 @@ output="trexio_vmc_COH2_gs"
 
 # Multicore test
 N=8
-ReferenceEnergy=-22.577964
-ReferenceError=0.0045168
+ReferenceEnergy=-22.5866185
+ReferenceError=0.0021779
 mpirun -np $N ../../../bin/vmc.mov1 -i ${input} -o ${output}_core_${N}.out -e error
 echo "Comparing energy with reference Core=${N}           (total E = $ReferenceEnergy +-  $ReferenceError ) "
 ../../../tools/compare_value.py ${output}_core_${N}.out     "total E"  $ReferenceEnergy     $ReferenceError
