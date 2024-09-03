92a5671cd0c0d429d5d23554d1829c26d306bc27
Ravindra Shinde <neelravi@users.noreply.github.com>
2024-09-03 09:15:09 +0200

    3-body Jastrow test added (#294)

---
+++ tests/CI_test/VMC-TREXIO-H2O-3body-Jastrow/VMC_TREXIO_H2O_3body_jastrow_water.sh
@@ -0,0 +1,12 @@
+echo "TREXIO water 3body Jastrow"
+
+input="vmc.inp"
+output="vmc_3body"
+
+# Multicore test
+N=2
+ReferenceEnergy=-17.2269659
+ReferenceError=0.0016067
+mpirun -np $N ../../../bin/vmc.mov1 -i ${input} -o ${output}_core_${N}.out -e error
+echo "Comparing energy with reference Core=${N}           (total E = $ReferenceEnergy +-  $ReferenceError ) "
+../../../tools/compare_value.py ${output}_core_${N}.out     "total E"  $ReferenceEnergy     $ReferenceError
