57008a167f140991e5fe30dde385a8741515f545
Ravindra Shinde <neelravi@users.noreply.github.com>
2024-09-20 12:13:10 +0200

    developers documentation part 1 (#296)
    * Documenting all variables and procedures from a new branch
    
    * cleanup first
    
    * [batch 3 of 100] developers documentation
    
    * [batch 4 of 100] developers documentation

--- src/module/m_atom.f90
+++ src/module/m_atom.f90
@@ -1,63 +1,76 @@
-!> Module that defines the sytem / molecule.
-!>@param nelec Number of electrons in the system.
-!>@param ndn Number of down electrons.
-!>@param nup Number of up electrons.
-!>@param cent Coordinates of the centers.
-!>@param znuc Nuclear charges.
-!>@param iwctype Type of the wavefunction.
-!>@param nctype Number of types of centers.
-!>@param ncent Number of centers.
-!>@param nctype_tot Total number of types of centers.
-!>@param ncent_tot Total number of centers.
-!>@param symbol Elemental symbol of the centers.
-!>@param atomtyp Type of the centers.
-!>@param newghostype New ghost type.
-!>@param nghostcent Number of ghost centers.
-!>@param allocate_atom Allocates memory for the system.
-!>@author Ravindra Shinde
+!> Module that defines the system/molecule.
 module system
     use precision_kinds, only: dp
 
     implicit none
 
-    integer  :: nelec
+    !> Number of electrons in the system.
+    integer :: nelec
+
+    !> Number of down electrons.
     integer :: ndn
+
+    !> Number of up electrons.
     integer :: nup
 
+    !> Coordinates of the centers.
     real(dp), dimension(:, :), allocatable :: cent
+
+    !> Nuclear charges.
     real(dp), dimension(:), allocatable :: znuc
 
+    !> Type of the wavefunction.
     integer, dimension(:), allocatable :: iwctype
-    integer :: nctype, ncent
-    integer :: nctype_tot, ncent_tot
+
+    !> Number of types of centers.
+    integer :: nctype
+
+    !> Number of centers.
+    integer :: ncent
+
+    !> Total number of types of centers.
+    integer :: nctype_tot
+
+    !> Total number of centers.
+    integer :: ncent_tot
+
+    !> Elemental symbol of the centers.
     character(len=2), dimension(:), allocatable :: symbol
+
+    !> Type of the centers.
     character(len=2), dimension(:), allocatable :: atomtyp
 
+    !> New ghost type.
     integer :: newghostype
+
+    !> Number of ghost centers.
     integer :: nghostcent
 
     private
-    public :: nelec
-    public   :: ndn, nup
-    public   :: znuc, cent, iwctype, nctype, ncent, ncent_tot, nctype_tot, symbol, atomtyp
-    public   :: allocate_atom, deallocate_atom
-    public   :: newghostype, nghostcent
+    public :: nelec, ndn, nup, znuc, cent, iwctype, nctype, ncent, ncent_tot, nctype_tot
+    public :: symbol, atomtyp, allocate_atom, deallocate_atom
+    public :: newghostype, nghostcent
     save
 
 contains
+
     !> Allocates memory for the system.
-    !>@param allocate_atom Allocates memory for the system.
-    !>@author Ravindra Shinde
     subroutine allocate_atom()
+
+        !> Allocating memory for centers.
         if (.not. allocated(cent)) allocate (cent(3, ncent_tot))
+
+        !> Allocating memory for nuclear charges.
         if (.not. allocated(znuc)) allocate (znuc(nctype_tot))
+
+        !> Allocating memory for wavefunction type.
         if (.not. allocated(iwctype)) allocate (iwctype(nctype_tot), source=0)
+
+        !> Allocating memory for elemental symbols.
         if (.not. allocated(symbol)) allocate (symbol(ncent_tot))
     end subroutine allocate_atom
 
     !> Deallocates memory for the system.
-    !>@param deallocate_atom Deallocates memory for the system.
-    !>@author Ravindra Shinde
     subroutine deallocate_atom()
         if (allocated(iwctype)) deallocate (iwctype)
         if (allocated(znuc)) deallocate (znuc)
@@ -65,4 +78,3 @@ contains
     end subroutine deallocate_atom
 
 end module system
-
