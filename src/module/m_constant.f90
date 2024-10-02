5c5cdab28cc6e7f2869f7dfbc0a1ae0b5402b095
Ravindra Shinde <neelravi@gmail.com>
2024-10-02 09:51:31 +0200

    second batch of documentation

--- src/module/m_constant.f90
+++ src/module/m_constant.f90
@@ -1,19 +1,30 @@
+
+!> Module containing esigma and esigmatrial
 module const
       use precision_kinds, only: dp
     implicit none
 
+    !> esigma
     real(dp) :: etrial
+
+    !> esigmatrial
     real(dp) :: esigmatrial
 
     save
 end module const
 
+!> Module containing physical constants
 module constants
       use precision_kinds, only: dp
     implicit none
 
+    !> Plank's constant
     real(dp), parameter :: hb = 0.5
+
+    !> Value of Pi
     real(dp), parameter :: pi = 4.0d0*datan(1.0d0)
+
+    !> Value of 2*Pi
     real(dp), parameter :: twopi = 8.d0*datan(1.0d0)
 
 end module
--- src/module/m_qmckl.F90
+++ src/module/m_qmckl.F90
@@ -2,11 +2,16 @@
 #include "qmckl_f.F90"
 #endif
 
+!> Module that contains the qmckl context
 module qmckl_data
 #ifdef QMCKL_FOUND
 
   use qmckl
+
+  !> QMCKL context
   integer(qmckl_context) :: qmckl_ctx
+
+  !> Logical to check if QMCKL is used
   logical                :: use_qmckl = .True.
 
   public :: qmckl_ctx, use_qmckl
@@ -14,8 +19,13 @@ module qmckl_data
 
 #else
 
+  !> QMCKL context
   integer :: qmckl_ctx = 0
+
+  !> Logical to check if QMCKL is successfully linked
   integer :: QMCKL_SUCCESS = 0
+
+  !> Logical to check if QMCKL is used
   logical :: use_qmckl = .False.
 
   public :: qmckl_ctx, use_qmckl, QMCKL_SUCCESS
