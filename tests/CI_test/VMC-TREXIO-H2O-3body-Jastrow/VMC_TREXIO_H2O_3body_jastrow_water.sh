deffa79c35e96b196820d748b541f0606af75c2a
Ravindra Shinde <neelravi@gmail.com>
2025-04-29 11:23:39 +0200

    move to Github hosted runner and GNU compilers requires reverification of reference values

--- tests/CI_test/VMC-TREXIO-H2O-3body-Jastrow/VMC_TREXIO_H2O_3body_jastrow_water.sh
+++ tests/CI_test/VMC-TREXIO-H2O-3body-Jastrow/VMC_TREXIO_H2O_3body_jastrow_water.sh
@@ -5,8 +5,8 @@ output="vmc_3body"
 
 # Multicore test
 N=2
-ReferenceEnergy=-17.2299543
-ReferenceError=0.001627
+ReferenceEnergy=-17.2252374
+ReferenceError=0.0015519
 mpirun -np $N ../../../bin/vmc.mov1 -i ${input} -o ${output}_core_${N}.out -e error
 echo "Comparing energy with reference Core=${N}           (total E = $ReferenceEnergy +-  $ReferenceError ) "
 ../../../tools/compare_value.py ${output}_core_${N}.out     "total E"  $ReferenceEnergy     $ReferenceError
