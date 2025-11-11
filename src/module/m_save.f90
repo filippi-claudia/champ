08c52cff8a0f74003d4148165572a1e024cf042b
Ravindra Shinde <neelravi@gmail.com>
2025-11-11 11:14:59 +0100

    07/48 doc for module m_save

--- src/module/m_save.f90
+++ src/module/m_save.f90
@@ -1,25 +1,87 @@
+!> @brief Module for saving and storing wavefunction parameters during optimization.
+!> @author CHAMP developers
+!> @date 2025
+!>
+!> @details This module stores current and best wavefunction parameters during optimization,
+!> including Jastrow parameters (a, b, c), CI/CSF coefficients, and determinant coefficients.
+!> It maintains both working copies and best-found parameters for backtracking and restart purposes.
 module save_mod
   use precision_kinds, only: dp
   implicit none
-  integer :: mparmja, mparmjb, mparmjc
+
+  !> Number of type-a Jastrow parameters.
+  integer :: mparmja
+
+  !> Number of type-b Jastrow parameters.
+  integer :: mparmjb
+
+  !> Number of type-c Jastrow parameters.
+  integer :: mparmjc
+
+  !> Saved type-a Jastrow parameters (4th-order).
   real(dp), allocatable :: a4_save(:,:,:)
+
+  !> Saved type-b Jastrow parameters.
   real(dp), allocatable :: b_save(:,:,:)
+
+  !> Saved type-c Jastrow parameters.
   real(dp), allocatable :: c_save(:,:,:)
+
+  !> Saved orbital coefficients.
   real(dp), allocatable :: coef_save(:,:,:)
+
+  !> Saved determinant coefficients.
   real(dp), allocatable :: cdet_save(:,:)
+
+  !> Saved CSF coefficients.
   real(dp), allocatable :: ccsf_save(:,:)
 
-  integer :: mparmja_best, mparmjb_best, mparmjc_best
+  !> Number of type-a Jastrow parameters for best wavefunction.
+  integer :: mparmja_best
+
+  !> Number of type-b Jastrow parameters for best wavefunction.
+  integer :: mparmjb_best
+
+  !> Number of type-c Jastrow parameters for best wavefunction.
+  integer :: mparmjc_best
+
+  !> Best type-a Jastrow parameters (4th-order).
   real(dp), allocatable :: a4_best(:,:,:)
+
+  !> Best type-b Jastrow parameters.
   real(dp), allocatable :: b_best(:,:,:)
+
+  !> Best type-c Jastrow parameters.
   real(dp), allocatable :: c_best(:,:,:)
+
+  !> Best orbital coefficients.
   real(dp), allocatable :: coef_best(:,:,:)
+
+  !> Best determinant coefficients.
   real(dp), allocatable :: cdet_best(:,:)
+
+  !> Best CSF coefficients.
   real(dp), allocatable :: ccsf_best(:,:)
 
-  integer :: nciterm_sav, norbterm_sav, nparmd_sav, nparmj_sav, nreduced_sav, nadorb_sav
+  !> Saved number of CI terms.
+  integer :: nciterm_sav
 
-  ! from dmc/nonloc_grid.f
+  !> Saved number of orbital terms.
+  integer :: norbterm_sav
+
+  !> Saved number of determinant parameters.
+  integer :: nparmd_sav
+
+  !> Saved number of Jastrow parameters.
+  integer :: nparmj_sav
+
+  !> Saved number of reduced parameters.
+  integer :: nreduced_sav
+
+  !> Saved number of active orbitals.
+  integer :: nadorb_sav
+
+  !> Saved pseudopotential matrix elements for nonlocal grids from dmc/nonloc_grid.f.
   real(dp), allocatable :: t_vpsp_save(:, :, :)
 
   save
