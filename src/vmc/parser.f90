8028221e7ead6d27216252e08045e1cf559d19e4
Emiel Slootman <e.slootman@utwente.nl>
2026-04-15 16:22:07 +0200

    Fix

--- src/vmc/parser.f90
+++ src/vmc/parser.f90
@@ -2036,8 +2036,10 @@ subroutine parser
 
 ! Done reading all the files
 
-! ! Not sure if this line should be here or not.
-! call pot_nn(cent,znuc,iwctype,ncent,pecent)
+! Required for restart forces (more work for periodic needed)
+  if (iperiodic.eq.0) then
+     call pot_nn(cent,znuc,iwctype,ncent,pecent)
+  endif
 
 ! Make sure that all the blocks are read. Use inputflags here to check
 
