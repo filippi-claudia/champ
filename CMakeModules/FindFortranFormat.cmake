77809f357eba936291ba872d2c08339581513010
Claudia Filippi <c.filippi@utwente.nl>
2026-05-15 06:33:52 +0200

    Remove unused CMake entries

--- CMakeModules/FindFortranFormat.cmake
+++
@@ -1,7 +0,0 @@
-
-# Add fixed format property
-macro (formatFortran SOURCE_FILES)
-  foreach(_source ${SOURCE_FILES})
-    set_property(SOURCE ${_source} PROPERTY Fortran_FORMAT FIXED)
-  endforeach()
-endmacro()
