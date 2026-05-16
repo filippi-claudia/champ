6b25798ad9f76f1b03973cf5782eb27713a16f97
Claudia Filippi <c.filippi@utwente.nl>
2026-05-16 22:29:52 +0200

    Rename VMC driver and clean main entry point

--- src/vmc/main.f90
+++ src/vmc/main.f90
@@ -13,19 +13,15 @@ module main_mod
     contains
 subroutine initialize_main
     !> Initialize MPI
-      use allocation_mod, only: deallocate_vmc
-      use contrl_file, only: close_files,init_logfile,init_procfile
-      use contrl_file, only: initialize,ounit
-      use control, only: init_control_mode
+      use contrl_file, only: init_logfile,init_procfile
+      use contrl_file, only: initialize
       use mpi
-      use mpiconf, only: idtask,mpiconf_init,nproc,wid
-      use mpitimer, only: elapsed_time,mpi_time,time_check1,time_final
+      use mpiconf, only: idtask,mpiconf_init,nproc
+      use mpitimer, only: elapsed_time,mpi_time,time_check1
       use mpitimer, only: time_start
-      use optwf_mod, only: optwf
       use parser_mod, only: parser
-      use precision_kinds, only: dp
 
-    implicit None
+    implicit none
     integer :: ierr
 
 
@@ -54,24 +50,24 @@ subroutine initialize_main
     !> Initiaize log check.XXX files. It needs ipr flag value.
     call init_procfile()
 
-    call elapsed_time("MPI Barrier before optwf : ")
+    call elapsed_time("Before VMC driver : ")
 
 end subroutine
 
 subroutine finalize_main()
     !> Finalize MPI
-    use mpi_f08
+    use mpi
     use allocation_mod, only: deallocate_vmc
-    use contrl_file,    only: ounit
+    use contrl_file,    only: close_files,ounit
     use mpitimer,    only: mpi_time, time_start, time_final
 
     implicit none
     integer :: ierr
-    ! call close_files()
     time_final = mpi_time()
 
     write(ounit,'(a,g16.6,a)') " REAL TIME (Total) of computation ::  ", time_final - time_start, " seconds "
 
+    call close_files()
     call mpi_finalize(ierr)
     call deallocate_vmc()
 end subroutine
@@ -82,16 +78,15 @@ end module
 program main
     !> Main program of CHAMP
     use main_mod, only: initialize_main, finalize_main
-    use optwf_mod, only: optwf
+    use vmc_driver_mod, only: run_vmc_driver
 
-    implicit None
-    integer :: ierr
+    implicit none
 
     !> Initialize MPI
     call initialize_main()
 
-    !> Run the optimization
-    call optwf()
+    !> Run plain VMC or wave-function optimization.
+    call run_vmc_driver()
 
     !> Finalize MPI
     call finalize_main()
